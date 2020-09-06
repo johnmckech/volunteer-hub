7e37f211612da187ed298102d918288b8df5727e
Jonathan <eimerarts@gmail.com>
2020-09-06 16:03:29 -0500

    login authentication

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS volunteer_hub_db;
+
+CREATE DATABASE volunteer_hub_db;
+
---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+// const passport = require("passport");
+const sequelize = require("./config/connection");
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user.js')
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }).then(user => {
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      console.log(user.password + ' vs ' + password)
+      if (!user.checkPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      done(null, user);
+    }).catch(err => done(err));
+  }
+));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(require('./routes/login'));
+// app.use(require('./config/passport'));
+
+var session = require("express-session"),
+    bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.use(session({ secret: "cats" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('./routes/login'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
