7a8a39e5c3a30a8a746b6f3d8c95f12a4327cdbb
John McK <john.mckech3@gmail.com>
2020-09-07 16:18:38 -0500

    other forms added

--- frontend.js
+++ frontend.js
@@ -18,8 +18,7 @@ document.querySelector("#submit").onclick = function(e){
     postLang(languages)
     
 }
-
-    function isChecked(param){
+     function isChecked(param){
         console.log({before:param})
         if($('#'+param).prop('checked')){
             param = true;
@@ -43,3 +42,45 @@ document.querySelector("#submit").onclick = function(e){
         }
         }) 
         }
+
+        document.querySelector("#submit").onclick = function(e){
+            e.preventDefault();
+        
+            var facebook="facebook"
+            var microsoft="microsoft";
+        
+            isChecked(facebook), isChecked(microsoft)
+        
+            var techKnowledge = {
+               fb:isChecked(facebook),
+               mo:isChecked(microsoft),
+            };
+        
+            postLang(techKnowledge)
+            
+        }
+        
+            function isChecked(param){
+                console.log({before:param})
+                if($('#'+param).prop('checked')){
+                    param = true;
+                }
+                else{
+                    param = false;
+                }
+                console.log({after:param})
+                return param;}
+            
+            function postLang(param){
+                console.log(param)
+                $.ajax({
+                url:"http://localhost:3000/"/*whatever the route is*/,
+                method:"POST",
+                dataType:"json",
+                data:param,
+                /*look up the AJAX function documentation on jquery's website to figure out how to send languages as a JSON object*/
+                success:function(){
+                    console.log("AJAX successful")
+                }
+                }) 
+                }
