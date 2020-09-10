1d6dc0499ea13dbd09eb0380dc3af4be70414335
Jonathan <eimerarts@gmail.com>
2020-09-10 10:54:05 -0500

    changes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const sequelize = require("./config/connection");
 const passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user.js')
+const db = require('./db/database');
+const inputCheck = require('./utils/inputCheck');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
@@ -55,27 +59,17 @@ app.use(require('./routes/login'));
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-=======
 
-const express = require('express');
-const db = require('./db/database');
-const inputCheck = require('./utils/inputCheck');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3030;
-const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+
 
 app.post('/',function(req,res){
     var mainReq = req.body
     console.log({mainReq})
 })
  
-app.listen(3000)
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -87,11 +81,5 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
-// Start server after DB connection
-db.on('open', () => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-});
 
 
