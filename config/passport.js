e97d34b25b619bc075e41049e015babf99566bab
John McK <john.mckech3@gmail.com>
2020-09-13 17:04:18 -0500

    file cleanup and 'user' mentions deleted

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,18 @@
 const passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user.js')
+const Volunteer = require('../models/')
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
+    Volunteer.findOne({ username: username }, function(err, volunteer) {
       if (err) { return done(err); }
-      if (!user) {
+      if (!volunteer) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
+      if (!volunteer.validPassword(password)) {
         return done(null, false, { message: 'Incorrect password.' });
       }
-      return done(null, user);
+      return done(null, volunteer);
     });
   }
 ));
